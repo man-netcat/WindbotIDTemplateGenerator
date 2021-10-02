1622a28dc95b8edb93f84704c19b1603c695ed41
Rick Teuthof <rmt22198@outlook.com>
2021-10-03 00:03:17 +0200

    Fixed writing to file and check for deck to exist

--- templategen.py
+++ templategen.py
@@ -27,13 +27,18 @@ def main():
     if len(sys.argv) != 3:
         print("usage: python3 templategen.py [executorname] [ydk path]")
         exit(1)
+        
+    executorname = sys.argv[1]
+    ydkfile = sys.argv[2]
+    
+    if not os.path.exists(ydkfile):
+        print("YDK file not found.")
+        exit(1)
 
     if not os.path.exists('./cards.cdb'):
         print("Put cards.cdb next to this script.")
         exit(1)
 
-    executorname = sys.argv[1]
-    ydkfile = sys.argv[2]
 
     con = sqlite3.connect('cards.cdb')
     idlist = ydk_to_idlist(ydkfile)
@@ -58,10 +63,10 @@ namespace WindBot.Game.AI.Decks
     public class {executorname}Executor : DefaultExecutor
     {{
         public class CardId
-        {{""")
+        {{\n""")
         for _, card in df.iterrows():
             f.write(
-                f"            public const int {clean_cardname(card['name'])} = {card['id']};")
+                f"            public const int {clean_cardname(card['name'])} = {card['id']};\n")
         f.write(f"""        }}
         public {executorname}Executor(GameAI ai, Duel duel) :
             base(ai, duel)
